76cf6872608ba5dfc4505ef6a491c5a821cb7dba
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-13 20:14:22 -0500

    Add database helper and GET endpoint to display a list of notes

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 
+const notes = require('../notes/notesHelper.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.get('/note/get/all', (req, res) => {
+  notes.getNotes()
+    .then(rows => res.status(200).json(rows))
+    .catch(err => res.status(500).json(err));
+});
 
 module.exports = server;
