015ce025f93349850a5b64080308dd73faf52be9
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-15 19:32:15 -0500

    Add database helpers

--- notes/notesHelper.js
+++ notes/notesHelper.js
@@ -4,4 +4,20 @@ module.exports = {
   getNotes: () => {
     return db('notes');
   },
+
+  getNoteById: (id) => {
+    return db('notes').where('id', id);
+  },
+
+  createNote: (note) => {
+    return db('notes').insert(note);
+  },
+
+  editNote: (note) => {
+    return db('notes').where('id', note.id).update(note);
+  },
+
+  deleteNote: (id) => {
+    return db('notes').where('id', id).del();
+  },
 };
