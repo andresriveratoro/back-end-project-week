8447243c32eea913ca75cae1cd1ecdd9cc29d56c
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-20 20:00:49 -0500

    Add front end from previous project

---
+++ front-end/src/components/EditNote.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+export default class EditNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      _id: null,
+      title: '',
+      textBody: '',
+    };
+  }
+
+  componentDidMount() {
+    const note = this.props.notes.find(
+      note => `${note._id}` === this.props.match.params.id
+    );
+    this.setState(note);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.title === '' ||
+      this.props.match.params.id !== prevProps.match.params.id
+    ) {
+      const note = this.props.notes.find(
+        note => `${note._id}` === this.props.match.params.id
+      );
+      this.setState(note);
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(
+      {
+        title: this.state.title,
+        textBody: this.state.textBody,
+      },
+      this.state._id
+    );
+    this.props.history.push('/');
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Edit Note:</h2>
+        <input
+          type="text"
+          name="title"
+          value={this.state.title}
+          placeholder="Note Title"
+          onChange={this.handleChange}
+          required
+        />
+        <textarea
+          name="textBody"
+          value={this.state.textBody}
+          placeholder="Note Content"
+          onChange={this.handleChange}
+          required
+        />
+        <button type="submit">Update</button>
+      </form>
+    );
+  }
+}
---
+++ front-end/src/components/ViewNote.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import EditModal from './EditModal';
+
+export default class ViewNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      _id: null,
+      title: '',
+      textBody: '',
+      show: false,
+    };
+  }
+
+  componentDidMount() {
+    const note = this.props.notes.find(
+      note => `${note._id}` === this.props.match.params.id
+    );
+    this.setState(note);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.title === '' ||
+      this.props.match.params.id !== prevProps.match.params.id
+    ) {
+      const note = this.props.notes.find(
+        note => `${note._id}` === this.props.match.params.id
+      );
+      this.setState(note);
+    }
+  }
+
+  deleteNote = () => {
+    this.props.deleteNote(this.state._id);
+    this.props.history.push('/');
+  };
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    return (
+      <div className="view-note">
+        <div className="view-note-actions">
+          <span><Link to={`/edit/${this.state._id}`}>edit</Link></span>
+          <span onClick={this.showModal}>delete</span>
+        </div>
+        <h2>{this.state.title}</h2>
+        <p>{this.state.textBody}</p>
+        {this.state.show ? (
+          <EditModal deleteNote={this.deleteNote} hideModal={this.hideModal} />
+        ) : null}
+      </div>
+    );
+  }
+}
